61d801988acbaa1222d06b779fb9c36685620907
Alvika Prahasta <alvikaajiandrianty@gmail.com>
2023-11-22 12:33:34 +0700

    validation in put wishlist

--- src/api/store/wishlist/wishlist-update.ts
+++ src/api/store/wishlist/wishlist-update.ts
@@ -1,21 +1,26 @@
 import { MedusaRequest, MedusaResponse } from "@medusajs/medusa";
+import { IsString, IsOptional } from "class-validator";
+import { validator } from "@medusajs/medusa";
 
 export const updateWishlist = async (req: MedusaRequest, res: MedusaResponse): Promise<void> => {
     try {
+        const validated = await validator(StorePostWishlistNameReq, req.body);
         const { id } = req.params;
 
         if (!id || typeof id !== 'string') {
             res.status(400).json({ message: "Invalid or missing id" });
             return;
         }
+
         const wishlistNameService = req.scope.resolve('wishlistNameService');
         const wishlistName = await wishlistNameService.retrieveWishlistName(id)
         if (!wishlistName) {
             res.status(404).json({ message: "Wishlist Name not found" });
             return;
         }
+
         if (req.user?.customer_id === wishlistName.customer_id) {
-            const payload = { title: req.body.title }
+            const payload = { title: validated.title }
             await wishlistNameService.updateWishlistName(id, payload)
             res.json({ message: 'Wishlist Updated' });
         } else {
@@ -26,3 +31,12 @@ export const updateWishlist = async (req: MedusaRequest, res: MedusaResponse): P
         res.status(500).json({ message: "Internal Server Error" });
     }
 };
+
+export class StorePostWishlistNameReq {
+    @IsOptional()
+    @IsString()
+    id?: string;
+
+    @IsString()
+    title: string;
+}
